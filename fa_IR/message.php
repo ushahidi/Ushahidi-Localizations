0a2b0692c8d1bd0c50ca7025eb3dc4c81eed2fb0
ushahidi <ushahidi@cloud-ushahidi.hspheredns.com>
2010-10-15 11:12:36 -0500

    Updating translations for فارسی (Iran, Islamic Republic of)

---
+++ fa_IR/message.php
@@ -0,0 +1,22 @@
+<?php
+	$lang = array(
+	'captcha' => array(
+		'default' => '',
+		'required' => '',
+	),
+	'email' => array(
+		'email' => '',
+		'length' => '',
+		'required' => '',
+	),
+	'message' => array(
+		'required' => '',
+	),
+	'name' => array(
+		'length' => '',
+		'required' => '',
+	),
+	'phone' => array(
+		'length' => '',
+	));
+?>
