aad7ab703faeb7e14c73150ae7ae6cc6fc7dad00
ushahidi <ushahidi@cloud-ushahidi.hspheredns.com>
2010-10-15 11:12:35 -0500

    Updating translations for Akan (Ghana)

---
+++ ak_GH/auth.php
@@ -0,0 +1,47 @@
+<?php
+	$lang = array(
+	'email' => array(
+		'email' => '',
+		'exists' => '',
+		'length' => '',
+		'required' => '',
+	),
+	'forgot_password' => '',
+	'name' => array(
+		'length' => '',
+		'required' => '',
+		'standard_text' => '',
+	),
+	'password' => array(
+		'alpha_numeric' => '',
+		'length' => '',
+		'login error' => '',
+		'matches' => '',
+		'required' => '',
+		'standard_text' => '',
+	),
+	'password_confirm' => array(
+		'matches' => '',
+	),
+	'resetemail' => array(
+		'email' => '',
+		'invalid' => '',
+		'required' => '',
+	),
+	'roles' => array(
+		'alpha_numeric' => '',
+		'length' => '',
+		'required' => '',
+		'values' => '',
+	),
+	'username' => array(
+		'admin' => '',
+		'alpha' => '',
+		'exists' => '',
+		'length' => '',
+		'login error' => '',
+		'required' => '',
+		'standard_text' => '',
+		'superadmin' => '',
+	));
+?>
---
+++ ak_GH/organization.php
@@ -0,0 +1,25 @@
+<?php
+	$lang = array(
+	'organization_description' => array(
+		'required' => '',
+	),
+	'organization_email' => array(
+		'email' => '',
+		'length' => '',
+	),
+	'organization_name' => array(
+		'length' => '',
+		'required' => '',
+		'standard_text' => '',
+	),
+	'organization_phone1' => array(
+		'length' => '',
+	),
+	'organization_phone2' => array(
+		'length' => '',
+	),
+	'organization_website' => array(
+		'required' => '',
+		'url' => '',
+	));
+?>
