ed889d0848e42703641feeab3c40a515b8510135
ushahidi <ushahidi@cloud-ushahidi.hspheredns.com>
2010-10-15 11:12:37 -0500

    Updating translations for Kiswahili (Kenya)

---
+++ sw_KE/auth.php
@@ -0,0 +1,47 @@
+<?php
+	$lang = array(
+	'email' => array(
+		'email' => 'Anwani ya barua pepe inaonekana kuwa na herufi zisizotakikana',
+		'exists' => 'Pole, Anwani ya barua pepe uliopatia ilishasajilishwa.',
+		'length' => 'Anwani ya barua pepe lazima iwe baina ya herufi 4 na 64.',
+		'required' => 'Anwani ya barua pepe inahitajika.',
+	),
+	'forgot_password' => '',
+	'name' => array(
+		'length' => 'Jina kwa ukamili lazima iwe baina ya herufi 3 na 100.',
+		'required' => 'Jina kwa ukamili linahitajika.',
+		'standard_text' => 'Jina la mtumizi limeandikwa kwa herufi zisizokubalika.',
+	),
+	'password' => array(
+		'alpha_numeric' => '',
+		'length' => 'Nywila lazima iwe baina ya herufi 5 na 16.',
+		'login error' => 'Tafadhali hakikisha umeandika nywila sahihi.',
+		'matches' => 'Tafadhali andika nywila zilofanana kwenye sehemu ya nywila na kuthibitisha nywila.',
+		'required' => 'Nywila inahitajika.',
+		'standard_text' => '',
+	),
+	'password_confirm' => array(
+		'matches' => 'Sehemu ya kuthibitisha nywila lazima iwe sawa na sehemu ya nywila.',
+	),
+	'resetemail' => array(
+		'email' => 'Anwani ya barua pepe inaonekana si sahihi',
+		'invalid' => 'Pole, hatuna anwani yako ya barua pepe',
+		'required' => 'Anwani ya barua pepe inahitajika.',
+	),
+	'roles' => array(
+		'alpha_numeric' => '',
+		'length' => '',
+		'required' => 'Lazima ufasili jukumu lako.',
+		'values' => 'Lazima uchague jukumu la MENEJA au MTUMIZI.',
+	),
+	'username' => array(
+		'admin' => 'Jukumu la meneja haliwezi kubadilishwa.',
+		'alpha' => '',
+		'exists' => 'Pole, Jina la mtumizi lilishasajilishwa.',
+		'length' => 'Jina la mtumizi lazma liwe na baina ya herufi 2 na 16.',
+		'login error' => 'Tafadhali hakikisha umeandika jina la mtumizi kisahihi.',
+		'required' => 'Jina la mtumizi linahitajika.',
+		'standard_text' => '',
+		'superadmin' => 'Jukumu la meneja haliwezi kubadilishwa.',
+	));
+?>
