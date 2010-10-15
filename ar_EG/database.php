426d84d11599c47d63d1797ce11b847ff59b723e
ushahidi <ushahidi@cloud-ushahidi.hspheredns.com>
2010-10-15 11:12:35 -0500

    Updating translations for العربية (Egypt)

---
+++ ar_EG/database.php
@@ -0,0 +1,6 @@
+<?php
+	$lang = array(
+	'error' => 'Database error: %s',
+	'table_not_found' => 'جدول "%s" لم يمكن إيجاده فى قاعدة البيانات. Please make sure you are using the latest version of the database for this version of Ushahidi',
+	);
+?>
---
+++ ar_EG/imap.php
@@ -0,0 +1,6 @@
+<?php
+	$lang = array(
+	'imap_stream_not_opened' => 'IMAP Stream لم نتمكن من تشغيل',
+	'unsupported_service' => 'خدمات البريد الإليكترونى غير متوفرة',
+	);
+?>
