98e25d36dc1712e019dc79045a86f42e878ac799
ushahidi <ushahidi@cloud-ushahidi.hspheredns.com>
2010-10-15 11:12:36 -0500

    Updating translations for 한국어 (韓國語), 조선말 (朝鮮語) (Korea, Republic of)

---
+++ ko_KR/comments.php
@@ -0,0 +1,19 @@
+<?php
+	$lang = array(
+	'captcha' => array(
+		'default' => '',
+		'required' => '',
+	),
+	'comment_author' => array(
+		'length' => '',
+		'required' => '',
+	),
+	'comment_description' => array(
+		'required' => '',
+	),
+	'comment_email' => array(
+		'email' => '',
+		'length' => '',
+		'required' => '',
+	));
+?>
---
+++ ko_KR/feeds.php
@@ -0,0 +1,15 @@
+<?php
+	$lang = array(
+	'date' => '날짜',
+	'feed_name' => array(
+		'length' => '',
+		'required' => '',
+	),
+	'feed_url' => array(
+		'required' => '',
+		'url' => '',
+	),
+	'source' => '',
+	'title' => '',
+	);
+?>
