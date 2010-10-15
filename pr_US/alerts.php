168bbb09c28b72e8c11b4ddb34844d266b7dd505
ushahidi <ushahidi@cloud-ushahidi.hspheredns.com>
2010-10-15 10:00:26 -0500

    First crack at committing legit files

---
+++ pr_US/alerts.php
@@ -0,0 +1,51 @@
+<?php
+	$lang = array(
+	'alert_email' => array(
+		'email' => 'This is a test',
+		'email_check' => 'Harrr!',
+		'length' => '',
+		'one_required' => '',
+		'required' => '',
+	),
+	'alert_lat' => array(
+		'between' => '',
+		'required' => '',
+	),
+	'alert_lon' => array(
+		'between' => '',
+		'required' => '',
+	),
+	'alert_mobile' => array(
+		'length' => '',
+		'mobile_check' => '',
+		'numeric' => '',
+		'one_required' => '',
+		'required' => '',
+	),
+	'alert_radius' => array(
+		'in_array' => '',
+		'required' => '',
+	),
+	'code_already_verified' => '',
+	'code_not_found' => '',
+	'code_verified' => '',
+	'confirm_request' => '',
+	'create_more_alerts' => '',
+	'email_alert_request_created' => '',
+	'email_code' => '',
+	'email_error_head' => '',
+	'email_ok_head' => '',
+	'error' => '',
+	'error_body' => '',
+	'mobile_alert_request_created' => '',
+	'mobile_code' => '',
+	'mobile_error_head' => '',
+	'mobile_ok_head' => '',
+	'settings_error' => '',
+	'unsubscribe' => '',
+	'unsubscribed' => '',
+	'unsubscribe_failed' => '',
+	'verification_email_subject' => '',
+	'verify_code' => '',
+	);
+?>
---
+++ pr_US/bug.php
@@ -0,0 +1,23 @@
+<?php
+	$lang = array(
+	'captcha' => array(
+		'default' => 'captcherrr*default',
+		'required' => 'Captcharrr+required',
+	),
+	'email' => array(
+		'email' => 'This is a test I\'ll test this I will - test this with Some $ funky Character * ddsad * # $ % ^ fjdskfdsl few ferw f \ \ \ \ \' \' \' \' ',
+		'length' => '',
+		'required' => '',
+	),
+	'error' => array(
+		'required' => '',
+	),
+	'subject' => array(
+		'length' => '',
+		'required' => '',
+	),
+	'yourname' => array(
+		'length' => '',
+		'required' => '',
+	));
+?>
