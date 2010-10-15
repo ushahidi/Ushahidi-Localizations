4ed0de153f2f079aa3798d798aa2c6a13c219f07
ushahidi <ushahidi@cloud-ushahidi.hspheredns.com>
2010-10-15 11:12:36 -0500

    Updating translations for español, castellano (Argentina)

---
+++ es_AR/feeds.php
@@ -0,0 +1,15 @@
+<?php
+	$lang = array(
+	'date' => '',
+	'feed_name' => array(
+		'length' => 'El campo nombre de feed debe tener por lo menos y no mÃ¡s de 70 caracteres de largo.',
+		'required' => 'Por favor ingrese el nombre del feed.',
+	),
+	'feed_url' => array(
+		'required' => 'Por favor ingrese la URL del feed.',
+		'url' => 'Por favor ingrese una URL vÃ¡lida. Ej. http://www.ushahidi.com',
+	),
+	'source' => '',
+	'title' => '',
+	);
+?>
---
+++ es_AR/upgrade.php
@@ -0,0 +1,18 @@
+<?php
+	$lang = array(
+	'upgrade' => array(
+		'between' => '',
+	),
+	'upgrade_continue_btn_text' => '',
+	'upgrade_db_btn_text' => '',
+	'upgrade_db_title' => '',
+	'upgrade_failed' => 'La actualizaci',
+	'upgrade_status' => '',
+	'upgrade_text_1' => 'Estas instrucciones lo guiar',
+	'upgrade_text_2' => 'Paso 1: Descarge la ',
+	'upgrade_text_3' => 'Paso 2: Utilizando Telnet, FTP, SSH u otro protocolo, reemplace los contenidos de los directorios del sistema por los de la versi',
+	'upgrade_text_4' => 'Paso 3: Utilizando un cliente de MySQL (ejemplo: phpMyAdmin) ejecute <code>upgrade.sql</code>. Este programa actualizar',
+	'upgrade_text_5' => 'Para una actualizaci',
+	'upgrading' => 'Actualizando',
+	);
+?>
