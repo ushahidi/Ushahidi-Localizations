7f14d6ac533c499d1467adffa8502949e9b4f26b
ushahidi <ushahidi@cloud-ushahidi.hspheredns.com>
2010-10-15 11:12:36 -0500

    Updating translations for français (France)

---
+++ fr_FR/category.php
@@ -0,0 +1,25 @@
+<?php
+	$lang = array(
+	'category_color' => array(
+		'length' => 'Le champ «couleur» doit comporter 6 caractères.',
+		'required' => 'Veuillez spécifier une couleur.',
+	),
+	'category_description' => array(
+		'required' => 'Veuillez indiquer une description.',
+	),
+	'category_image' => array(
+		'size' => '',
+		'type' => '',
+		'valid' => '',
+	),
+	'category_title' => array(
+		'length' => 'Le titre doit comporter entre 3 et 80 caractères.',
+		'required' => 'Veuillez indiquer un titre.',
+	),
+	'parent_id' => array(
+		'exists' => '',
+		'numeric' => '',
+		'required' => '',
+		'same' => '',
+	));
+?>
---
+++ fr_FR/feeds.php
@@ -0,0 +1,15 @@
+<?php
+	$lang = array(
+	'date' => '',
+	'feed_name' => array(
+		'length' => 'Le nom du fil doit comporter entre 3 et 70 caractères.',
+		'required' => 'Entrez le nom du fil.',
+	),
+	'feed_url' => array(
+		'required' => 'Veuillez entrer l\'URL du fil.',
+		'url' => 'Veuillez entrer un URL valide. Ex: http://www.ushahidi.com',
+	),
+	'source' => '',
+	'title' => '',
+	);
+?>
