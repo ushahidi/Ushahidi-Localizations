6271ac4ccff9f7e6eac9e11dd888a49449a70b23
ushahidi <ushahidi@cloud-ushahidi.hspheredns.com>
2010-10-15 11:24:22 -0500

    Updating translations for اردو (Pakistan)

---
+++ ur_PK/form.php
@@ -0,0 +1,40 @@
+<?php
+	$lang = array(
+	'field_default' => array(
+		'length' => '',
+	),
+	'field_height' => array(
+		'between' => '',
+	),
+	'field_isdate' => array(
+		'between' => '',
+		'required' => '',
+	),
+	'field_name' => array(
+		'length' => '',
+		'required' => '',
+	),
+	'field_required' => array(
+		'between' => '',
+		'required' => '',
+	),
+	'field_type' => array(
+		'numeric' => '',
+		'required' => '',
+	),
+	'field_width' => array(
+		'between' => '',
+	),
+	'form_description' => array(
+		'required' => '',
+	),
+	'form_id' => array(
+		'default' => '',
+		'numeric' => '',
+		'required' => '',
+	),
+	'form_title' => array(
+		'length' => '',
+		'required' => '',
+	));
+?>
