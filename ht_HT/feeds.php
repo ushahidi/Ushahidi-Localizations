60a76f2d265c643746ed89ab17f7e024aa61d72a
ushahidi <ushahidi@cloud-ushahidi.hspheredns.com>
2010-10-18 08:51:34 -0500

    Updating translations for Kreyòl ayisyen (Haiti)

---
+++ ht_HT/feeds.php
@@ -0,0 +1,15 @@
+<?php
+	$lang = array(
+	'date' => '',
+	'feed_name' => array(
+		'length' => '',
+		'required' => '',
+	),
+	'feed_url' => array(
+		'required' => '',
+		'url' => '',
+	),
+	'source' => '',
+	'title' => '',
+	);
+?>
