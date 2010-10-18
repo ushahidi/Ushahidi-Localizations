9ea1a6c85582cfaecbabdc82caf7cec1fe2cac4d
ushahidi <ushahidi@cloud-ushahidi.hspheredns.com>
2010-10-18 11:47:13 -0500

    Updating translations for اردو (Pakistan)

--- ur_PK/alerts.php
+++ ur_PK/alerts.php
@@ -1,49 +1,4 @@
 <?php
 	$lang = array(
-	'alert_email' => array(
-		'email' => '',
-		'email_check' => '',
-		'length' => '',
-		'required' => '',
-	),
-	'alert_lat' => array(
-		'between' => '',
-		'required' => '',
-	),
-	'alert_lon' => array(
-		'between' => '',
-		'required' => '',
-	),
-	'alert_mobile' => array(
-		'length' => '',
-		'mobile_check' => '',
-		'numeric' => '',
-		'one_required' => '',
-		'required' => '',
-	),
-	'alert_radius' => array(
-		'in_array' => '',
-		'required' => '',
-	),
-	'code_already_verified' => '',
-	'code_not_found' => '',
-	'code_verified' => '',
-	'confirm_request' => '',
-	'create_more_alerts' => '',
-	'email_alert_request_created' => '',
-	'email_code' => '',
-	'email_error_head' => '',
-	'email_ok_head' => '',
-	'error' => '',
-	'mobile_alert_request_created' => '',
-	'mobile_code' => '',
-	'mobile_error_head' => '',
-	'mobile_ok_head' => '',
-	'settings_error' => '',
-	'unsubscribe' => '',
-	'unsubscribed' => '',
-	'unsubscribe_failed' => '',
-	'verification_email_subject' => '',
-	'verify_code' => '',
 	);
 ?>
--- ur_PK/auth.php
+++ ur_PK/auth.php
@@ -1,45 +1,4 @@
 <?php
 	$lang = array(
-	'email' => array(
-		'email' => '',
-		'exists' => '',
-		'length' => '',
-		'required' => '',
-	),
-	'name' => array(
-		'length' => '',
-		'required' => '',
-		'standard_text' => '',
-	),
-	'password' => array(
-		'alpha_numeric' => '',
-		'length' => '',
-		'login error' => '',
-		'matches' => '',
-		'required' => '',
-		'standard_text' => '',
-	),
-	'password_confirm' => array(
-		'matches' => '',
-	),
-	'resetemail' => array(
-		'email' => '',
-		'invalid' => '',
-		'required' => '',
-	),
-	'roles' => array(
-		'alpha_numeric' => '',
-		'length' => '',
-		'required' => '',
-		'values' => '',
-	),
-	'username' => array(
-		'admin' => '',
-		'alpha' => '',
-		'exists' => '',
-		'length' => '',
-		'login error' => '',
-		'required' => '',
-		'superadmin' => '',
-	));
+	);
 ?>
