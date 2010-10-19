3b70e12941d264ca257c84e5658c615b3bba4690
ushahidi <ushahidi@cloud-ushahidi.hspheredns.com>
2010-10-18 21:09:58 -0500

    Updating translations for Magyar (Hungary) with contributions by cs...@....eu

--- hu_HU/tooltips.php
+++ hu_HU/tooltips.php
@@ -1,4 +1,17 @@
 <?php
 	$lang = array(
+	'profile_email' => 'Az email címed',
+	'profile_name' => 'A teljes neved',
+	'profile_notify' => 'Ha az IGEN-t választod, akkor emailben kapsz értesítőt, amikor új jelentés vagy megjegyzés kerül beküldésre a websiteodra',
+	'profile_password' => 'A jelszavad. Hagyd üresen, ha meg szeretnéd tartani a jelenlegit.',
+	'profile_username' => 'A felhasználói nevedet nem tudod megváltoztatni.',
+	'settings_alert_email' => 'Ezt az email címet fogjuk használni, hogy értesítéseket küldjünk',
+	'settings_allow_comments' => 'A felhasználók megjegyzéseket fűzhetnek a főoldalon publikált jelentésekhez.',
+	'settings_allow_feed' => 'RSS csatornák megjelenítésének engedélyezés a főoldalon.',
+	'settings_allow_reports' => 'A felhasználók a webforms segítségével küldhetnek információt.',
+	'settings_configure_map' => 'Egy meghatározott helyre beállítja a térképet',
+	'settings_default_category_colors' => 'Egy színkód kiválasztása az összes kategóriára a site-on',
+	'settings_display_contact' => 'A kapcsolati fül be és kikapcsolása a főoldalon.',
+	'settings_server_username' => 'Ez az emailcím fogadja a jelentéseket.',
 	);
 ?>
