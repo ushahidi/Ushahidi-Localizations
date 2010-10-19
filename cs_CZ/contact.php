90b647636757e08cea3917676f2aff485f7afd75
ushahidi <ushahidi@cloud-ushahidi.hspheredns.com>
2010-10-19 12:06:07 -0500

    Updating translations for français (France)

--- cs_CZ/contact.php
+++ cs_CZ/contact.php
@@ -1,4 +1,23 @@
 <?php
 	$lang = array(
-	);
+	'captcha' => array(
+		'default' => 'Vložte platný ověřovací kód',
+		'required' => 'Napište zde ověřovací kód',
+	),
+	'contact_email' => array(
+		'email' => 'Emailová adresa není ve správném formátu',
+		'length' => 'Emailová adresa má mít minimálně 4 a maximálně 64 znaků.',
+		'required' => 'Vyplňte Váš email.',
+	),
+	'contact_message' => array(
+		'required' => 'Vyplňte vaši zprávu.',
+	),
+	'contact_name' => array(
+		'length' => 'Jméno má mít alespoň tři znaky.',
+		'required' => 'Vyplňte vaše jméno.',
+	),
+	'contact_subject' => array(
+		'length' => 'Předmět zprávy má mít alespoň tři znaky.',
+		'required' => 'Vyplňte předmět zprávy.',
+	));
 ?>
--- cs_CZ/datetime.php
+++ cs_CZ/datetime.php
@@ -1,4 +1,31 @@
 <?php
 	$lang = array(
-	);
+	'friday' => array(
+		'abbv' => 'Pá',
+		'full' => 'Pátek',
+	),
+	'monday' => array(
+		'abbv' => 'Po',
+		'full' => 'Pondělí',
+	),
+	'saturday' => array(
+		'abbv' => 'So',
+		'full' => 'Sobota',
+	),
+	'sunday' => array(
+		'abbv' => 'Ne',
+		'full' => 'Neděle',
+	),
+	'thursday' => array(
+		'abbv' => 'Čt',
+		'full' => 'Čtvrtek',
+	),
+	'tuesday' => array(
+		'abbv' => 'Út',
+		'full' => 'Úterý',
+	),
+	'wednesday' => array(
+		'abbv' => 'Stř',
+		'full' => 'Středa',
+	));
 ?>
