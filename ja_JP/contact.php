a3f5842df567c7d0cc967ff5f2cd311a32fe1fac
ushahidi <ushahidi@cloud-ushahidi.hspheredns.com>
2010-10-18 21:09:58 -0500

    Updating translations for 日本語 (にほんご／にっぽんご) (Japan) with contributions by ay...@...com

--- ja_JP/contact.php
+++ ja_JP/contact.php
@@ -1,4 +1,23 @@
 <?php
 	$lang = array(
-	);
+	'captcha' => array(
+		'default' => '有効なセキュリティコードを入力してください。',
+		'required' => 'セキュリティコードを入力してください。',
+	),
+	'contact_email' => array(
+		'email' => '正しいメールアドレス形式で入力してください。',
+		'length' => 'メールアドレスは最低4文字、最高64文字の長さにしてください。',
+		'required' => 'チェックボックスにチェックが入っている場合は、メールアドレスは必須です。',
+	),
+	'contact_message' => array(
+		'required' => 'メッセージは必須項目です。',
+	),
+	'contact_name' => array(
+		'length' => '名前は最低3文字上入力してください。',
+		'required' => '名前は必須項目です。',
+	),
+	'contact_subject' => array(
+		'length' => 'タイトルは最低3文字以上入力してください',
+		'required' => 'タイトルは必須項目です。',
+	));
 ?>
