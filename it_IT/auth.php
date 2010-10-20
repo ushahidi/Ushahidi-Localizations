38506d5f0566fcc56bf8e2a869da50cb33847d4c
ushahidi <ushahidi@cloud-ushahidi.hspheredns.com>
2010-10-20 12:23:41 -0500

    Updating translations for Italiano (Italy) with contributions by e....@...com

--- it_IT/auth.php
+++ it_IT/auth.php
@@ -2,41 +2,43 @@
 	$lang = array(
 	'email' => array(
 		'email' => 'Il campo email non contiene un indirizzo email valido',
-		'exists' => 'Siamo spiacenti, esiste uno user account con questa mail',
+		'exists' => 'Siamo spiacenti, esiste uno user account con questo indirizzo email.',
 		'length' => 'Il campo email deve essere lungo almeno 3 caratteri e non più di 64.',
-		'required' => 'Il campo email ',
+		'required' => 'Il campo email è richiesto.',
 	),
 	'name' => array(
-		'length' => 'Il campo nome deve essere lungo almeno 3 caratteri e non pi',
+		'length' => 'Il campo nome deve essere lungo almeno 3 caratteri e non più di 100.',
 		'required' => 'Il campo nome ',
 		'standard_text' => 'Il campo username contiene carattere non permessi.',
 	),
 	'password' => array(
 		'alpha_numeric' => 'La password deve contenere solo numeri o lettere.',
-		'length' => 'Il campo password deve essere lungo alemno 5 caratteri e non pi',
+		'length' => 'Il campo password deve essere lungo alemno 5 caratteri e non più di 16.',
 		'login error' => 'Per favore controlla che la password inserita sia corretta.',
 		'matches' => 'Per favore inserisci la stessa password nei due campi password.',
-		'required' => 'Il campo password ',
+		'required' => 'Il campo password è richiesto.',
 	),
 	'password_confirm' => array(
 		'matches' => 'Il campo password di conferma deve essere uguale al campo password.',
 	),
 	'resetemail' => array(
 		'email' => 'Il campo email non contiene un indirizzo email valido',
-		'invalid' => 'Spiacenti, il tuo indirizzo email non ',
-		'required' => 'Il campo email ',
+		'invalid' => 'Spiacenti, non abbiamo il tuo indirizzo email.',
+		'required' => 'Il campo email è richiesto.',
 	),
 	'roles' => array(
+		'alpha_numeric' => 'Formato del ruolo non valido.',
+		'length' => 'Il campo ruolo deve essere lungo almeno 5 caratteri e non più di 30.',
 		'required' => 'Devi definire almeno un ruolo.',
 		'values' => 'Seleziona il ruolo ADMIN or USER.',
 	),
 	'username' => array(
-		'admin' => 'Il ruolo admin dell\'utente non pu',
+		'admin' => 'Il ruolo admin dell\'utente non può essere modificato.',
 		'alpha' => 'Il campo username deve contenere solo lettere.',
-		'exists' => 'Spiacenti, questo username ',
-		'length' => 'Il campo username deve essere lungo almeno 2 caratteri e non pi',
+		'exists' => 'Spiacenti, questo username è già in uso.',
+		'length' => 'Il campo username deve essere lungo almeno 2 caratteri e non più di 30.',
 		'login error' => 'Per favore controlla che lo username inserito sia corretto.',
-		'required' => 'Il campo username ',
+		'required' => 'Il campo username è richiesto.',
 		'superadmin' => 'Il ruolo super admin non pu',
 	));
 ?>
--- it_IT/feedback.php
+++ it_IT/feedback.php
@@ -3,7 +3,7 @@
 	'feedback_actions' => 'Azioni',
 	'feedback_captcha' => array(
 		'required' => 'Ti invitiamo a inserire il Codice di Sicurezza.',
-		'valid' => 'Ti invitiamo a inserire un codice di sicurezza valido',
+		'valid' => 'Ti invitiamo a inserire un codice di sicurezza valido.',
 	),
 	'feedback_date' => 'Data',
 	'feedback_details' => 'Dettagli Feedback',
@@ -11,12 +11,12 @@
 		'required' => 'Ti invitiamo a inserire qualche informazione per la risposta.',
 	),
 	'feedback_title' => array(
-		'length' => 'il campo Titolo deve essere lungo almeno 3 caratteri e non pi',
-		'required' => 'Il Titolo ',
+		'length' => 'il campo Titolo deve essere lungo almeno 3 caratteri e non più di 100.',
+		'required' => 'Il Titolo è rischiesto.',
 	),
 	'person_email' => array(
 		'email' => 'Il campo email non contiene un indirizzo email valido?',
-		'required' => 'Ti invitiamo a inserire il tuo indirizzo email',
+		'required' => 'Ti invitiamo a inserire il tuo indirizzo email.',
 	),
 	'person_name' => array(
 		'required' => 'Ti invitiamo a inserire il tuo nome completo.',
