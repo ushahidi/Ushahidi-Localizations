61a20bbf6d5b5583500616f1f64268fac64953b8
ushahidi <ushahidi@cloud-ushahidi.hspheredns.com>
2010-10-20 12:23:41 -0500

    Updating translations for Pirate Yarr! (United States) with contributions by br...@...com

--- pr_US/auth.php
+++ pr_US/auth.php
@@ -1,4 +1,43 @@
 <?php
 	$lang = array(
-	);
+	'email' => array(
+		'email' => 'è,ò,ù,à',
+		'exists' => 'è,ò,ù,à',
+		'length' => 'è,ò,ù,à',
+		'required' => 'è,ò,ù,à',
+	),
+	'name' => array(
+		'length' => 'è,ò,ù,à',
+		'required' => 'è,ò,ù,à',
+		'standard_text' => 'è,ò,ù,à',
+	),
+	'password' => array(
+		'alpha_numeric' => 'è,ò,ù,à',
+		'length' => 'è,ò,ù,à',
+		'matches' => 'è,ò,ù,à',
+		'required' => 'è,ò,ù,à',
+		'standard_text' => 'è,ò,ù,à',
+	),
+	'password_confirm' => array(
+		'matches' => 'è,ò,ù,à',
+	),
+	'resetemail' => array(
+		'email' => 'è,ò,ù,à',
+		'invalid' => 'è,ò,ù,à',
+		'required' => 'è,ò,ù,à',
+	),
+	'roles' => array(
+		'alpha_numeric' => 'è,ò,ù,à',
+		'length' => 'è,ò,ù,à',
+		'required' => 'è,ò,ù,à',
+		'values' => 'è,ò,ù,à',
+	),
+	'username' => array(
+		'admin' => 'è,ò,ù,à',
+		'alpha' => 'è,ò,ù,à',
+		'exists' => 'è,ò,ù,à',
+		'length' => 'è,ò,ù,à',
+		'required' => 'è,ò,ù,à',
+		'superadmin' => 'è,ò,ù,à',
+	));
 ?>
--- pr_US/message.php
+++ pr_US/message.php
@@ -1,4 +1,6 @@
 <?php
 	$lang = array(
-	);
+	'captcha' => array(
+		'default' => 'è,ò,ù,à',
+	));
 ?>
