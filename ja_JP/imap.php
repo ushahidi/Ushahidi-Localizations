36892141962e7e5f563a58b23774ec66ca1e5d0a
ushahidi <ushahidi@cloud-ushahidi.hspheredns.com>
2010-10-27 13:30:47 -0500

    Updating translations for 日本語 (にほんご／にっぽんご) (Japan) with contributions by ay...@...com

--- ja_JP/imap.php
+++ ja_JP/imap.php
@@ -1,4 +1,6 @@
 <?php
 	$lang = array(
+	'imap_stream_not_opened' => 'IMAPストリームを開けません。',
+	'unsupported_service' => 'メールサービスはサポートされていません。',
 	);
 ?>
--- ja_JP/installer.php
+++ ja_JP/installer.php
@@ -1,4 +1,33 @@
 <?php
 	$lang = array(
+	'base_path' => 'ベースパス',
+	'database' => 'データベース',
+	'database_host' => 'データベースホスト',
+	'database_name' => 'データベース名',
+	'database_name_description' => 'Ushahidi を稼働させたいデータベース名は',
+	'default_language' => 'デフォルト言語(ロケール)',
+	'disable' => '無効',
+	'enable' => '有効',
+	'finished' => '終了',
+	'general' => 'ジェネラル',
+	'google_key' => '<span>Google</span> API Key',
+	'go_back' => '戻る',
+	'index' => array(
+		'advanced' => 'アドバンス インストール',
+		'basic_installation' => '基本インストール',
+	),
+	'mail_server' => 'メールサービス',
+	'mail_server_host' => 'メールサービスホスト',
+	'mail_server_password' => 'メールサーバーパスワード',
+	'mail_server_port' => 'メールサーバーポート',
+	'mail_server_type' => 'メールサーバー種別',
+	'map' => '地図',
+	'map_provider' => '地図提供者',
+	'password' => 'パスワード',
+	'password_description' => 'データベースのパスワード',
+	'previous' => '前',
+	'restart_apache' => 'Apacheを再起動してください。',
+	'select_mail_server_ssl' => 'SSL の有効化 もしくは　無効化',
+	'site_email' => 'サイトメールアドレス',
 	);
 ?>
