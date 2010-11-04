3782c429fa34b454e07dda18e6d53789559d66c0
ushahidi <ushahidi@cloud-crowdmap.hspheredns.com>
2010-11-04 18:21:42 -0500

    Updating translations for česky, čeština (Czech Republic) with contributions by in...@....cz

--- cs_CZ/mhi.php
+++ cs_CZ/mhi.php
@@ -1,4 +1,18 @@
 <?php
 	$lang = array(
-	);
+	'contact_captcha' => array(
+		'default' => 'Vložte, prosím, platný ověřovací kód.',
+		'required' => 'Vložte, prosím, ověřovací kód.',
+	),
+	'contact_email' => array(
+		'email' => 'Zadali jste správnou emailovou adresu?',
+		'required' => 'Zadejte platnou emailovou adresu',
+	),
+	'contact_message' => array(
+		'required' => 'Políčko se zprávou je povinná položka.',
+	),
+	'contact_subject' => array(
+		'length' => 'Políčko předmět musí mít alespoň tři znaky.',
+		'required' => 'Políčko předmět je povinnou položkou.',
+	));
 ?>
--- cs_CZ/notifications.php
+++ cs_CZ/notifications.php
@@ -1,4 +1,21 @@
 <?php
 	$lang = array(
-	);
+	'admin_footer' => 'Z vašeho webu byla zaslána tato zpráva',
+	'admin_login_url' => 'Login admina',
+	'admin_new_comment' => array(
+		'message' => 'Na webu byl zadán nový komentář v odpověď na:',
+		'subject' => 'Nový komentář',
+	),
+	'admin_new_email' => array(
+		'message' => 'Na web byla zaslána nová emailová zpráva',
+		'subject' => 'Nová emailová zpráva',
+	),
+	'admin_new_report' => array(
+		'message' => 'Nová zpráva byla zadána přes webové rozhraní',
+		'subject' => 'Nová zpráva',
+	),
+	'admin_new_sms' => array(
+		'message' => 'Na web byla zaslána nová sms zpráva.',
+		'subject' => 'Nová sms zpráva',
+	));
 ?>
