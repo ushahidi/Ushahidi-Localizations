ee056436795c8e24e72cdefbd7a91398db718dc2
ushahidi <ushahidi@cloud-ushahidi.hspheredns.com>
2010-11-23 15:11:42 -0600

    Updating translations for Akan (Ghana)

--- hu_HU/reporters.php
+++ hu_HU/reporters.php
@@ -4,6 +4,10 @@
 		'between' => 'A földrajzi szélesség mező nem tartalmaz valós szélességi fokot.',
 		'required' => 'A szélességi fok megadása kötelező! Kérem, kattintson a térképre a pontos helymeghatározáshoz.',
 	),
+	'level_id' => array(
+		'numeric' => 'A jelentési szint mező nem tartalmaz valós értéket.',
+		'required' => 'A Jelentési szint mező nem tartalmaz valós értéket.',
+	),
 	'location_name' => array(
 		'length' => 'A helyzetmegnevezés mező legalább 3 legfeljebb 200 karaktert tartalmazhat.',
 		'required' => 'A helyzetmegnevezés mező kitöltése kötelező.',
@@ -13,7 +17,7 @@
 		'required' => 'A hosszúsági fok megadása kötelező. Kérem, kattintson a térképre a pontos helymeghatározáshoz!',
 	),
 	'reporter_id' => array(
-		'numeric' => 'Érvénytelen ',
-		'required' => 'Érvénytelen ',
+		'numeric' => 'Érvénytelen Bejelentő.',
+		'required' => 'Érvénytelen Bejelentő.',
 	));
 ?>
--- pr_US/alerts.php
+++ pr_US/alerts.php
@@ -3,5 +3,8 @@
 	'alert_email' => array(
 		'email' => 'This is a test',
 		'email_check' => 'Harrr!',
+	),
+	'alert_radius' => array(
+		'in_array' => 'Yarr! Ye can\'t make a circle that big!',
 	));
 ?>
