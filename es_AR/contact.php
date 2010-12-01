98ce3b6b5c983938eea34012441b7e570c770de4
Omar <ovruni@riseup.net>
2010-12-01 06:48:40 -0500

    Language correxion in front-end (english and spanish)

--- es_AR/contact.php
+++ es_AR/contact.php
@@ -1,23 +1,23 @@
 <?php
 	$lang = array(
 	'captcha' => array(
-		'default' => 'Please enter a valid security code',
-		'required' => 'Please enter the security code',
+		'default' => 'Por favor, introduzca un código de seguridad válido',
+		'required' => 'Por favor, introduzca el código de seguridad',
 	),
 	'contact_email' => array(
-		'email' => 'The Email field does not appear to contain a valid email address?',
-		'length' => 'The Email field must be at least 4 and no more 64 characters long.',
-		'required' => 'The Email field is required if the checkbox is checked.',
+		'email' => 'El campo de correo electrónico no parece contener una dirección válida de correo electrónico?',
+		'length' => 'El campo correo electrónico debe tener por lo menos 4 y no más de 64 caracteres de longitud.',
+		'required' => 'El campo correo electrónico es obligatorio si la casilla está marcada.',
 	),
 	'contact_message' => array(
-		'required' => 'The message field is required.',
+		'required' => 'El campo mensaje es obligatorio.',
 	),
 	'contact_name' => array(
-		'length' => 'The name field must be at least 3 characters long.',
-		'required' => 'The name field is required.',
+		'length' => 'El campo nombre debe tener por lo menos 3 caracteres.',
+		'required' => 'El campo nombre es obligatorio.',
 	),
 	'contact_subject' => array(
-		'length' => 'The subject field must be at least 3 characters long.',
-		'required' => 'The subject field is required.',
+		'length' => 'El campo asunto debe tener por lo menos 3 caracteres.',
+		'required' => 'El campo asunto es obligatorio.',
 	));
 ?>
--- es_AR/feeds.php
+++ es_AR/feeds.php
@@ -1,5 +1,6 @@
 <?php
 	$lang = array(
+	'date' => 'FECHA',
 	'feed_name' => array(
 		'length' => 'El campo nombre de feed debe tener por lo menos y no mÃ¡s de 70 caracteres de largo.',
 		'required' => 'Por favor ingrese el nombre del feed.',
@@ -7,5 +8,8 @@
 	'feed_url' => array(
 		'required' => 'Por favor ingrese la URL del feed.',
 		'url' => 'Por favor ingrese una URL vÃ¡lida. Ej. http://www.ushahidi.com',
-	));
+	),
+        'source' => 'FUENTE',
+	'title' => 'TÍTULO',
+	);
 ?>
