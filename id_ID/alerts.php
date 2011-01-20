8b72c824f2fcca8f0990b31b78a5bffd1a4149ef
ext-rrbaker <ext-rrbaker@cloud-ushahidi.hspheredns.com>
2011-01-20 13:18:16 -0600

    Updating translations for Bahasa Indonesia (Indonesia) with contributions by ju...@...com

--- id_ID/alerts.php
+++ id_ID/alerts.php
@@ -1,4 +1,50 @@
 <?php
 	$lang = array(
+	'alert_email' => array(
+		'email' => 'Format alamat e-mail yang tercantum pada isian e-mail tidak valid ?',
+		'email_check' => 'Alamat e-mail yang tercantum sudah terdaftar sebagai penerima berita untuk lokasi tersebut',
+		'length' => 'Panjang isian alamat e-mail harus minimal 4 dan tidak lebih dari 64 karakter.',
+		'one_required' => '?',
+		'required' => 'Jika checkbox dicentang, isian e-mail tidak boleh kosong',
+	),
+	'alert_lat' => array(
+		'between' => 'Anda belum memilih lokasi yang valid pada peta',
+		'required' => 'Anda belum memilih lokasi yang valid pada peta',
+	),
+	'alert_lon' => array(
+		'between' => 'Anda belum memilih lokasi yang valid pada peta',
+		'required' => 'Anda belum memilih lokasi yang valid pada peta',
+	),
+	'alert_mobile' => array(
+		'length' => 'Jumlah karakter pada isian Telepon Genggam salah.',
+		'mobile_check' => 'Nomor telpon genggam yang dicantumkan sudah terdaftar untuk menerima berita dari lokasi itu.',
+		'numeric' => 'Nomor telepon genggam pada isian telepon genggam salah. Mohon isi hanya dengan angka termasuk kode negara.',
+		'one_required' => 'Anda harus mencantumkan nomor telepon genggam anda atau alamat e-mail anda',
+		'required' => 'Jika checkbox dicentang, isian telepon genggam tidak boleh kosong',
+	),
+	'alert_radius' => array(
+		'in_array' => 'Anda belum menentukan radius yang valid pada peta',
+		'required' => 'Anda belum menentukan radius anda pada peta',
+	),
+	'code_already_verified' => 'Kode ini sudah pernah diverifikasi !',
+	'code_not_found' => 'Kode verifikasi ini tidak ditemukan ! Mohon periksa kembali URL yang digunakan.',
+	'code_verified' => 'Kode anda telah diverifikasi dengan benar. Anda akan menerima pesan begitu ada kejadian.',
+	'confirm_request' => 'Untuk mengkonfirmasikan permintaan pesan anda, mohon lanjut ke',
+	'create_more_alerts' => 'Kembali ke laman pesan untuk membuat pesan-pesan baru',
+	'email_alert_request_created' => 'Permintaan notifikasi e-mail anda telah dibuat dan pesan untuk memverifikasi ini telah di kirim ke',
+	'email_code' => 'Silakan masukan kode konfirmasi e-mail yang anda terima di bawah ini :',
+	'email_error_head' => 'Permintaan notifikasi e-mail anda belum tersimpan !',
+	'email_ok_head' => 'Permintaan notifikasi e-mail anda telah tersimpan!',
+	'error' => 'Sistem tidak bisa memroses permintaan konfirmasi anda!',
+	'mobile_alert_request_created' => 'Permintaan notifikasi mobile anda telah dibuat dan pesan untuk memverifikasi ini telah dikirim ke',
+	'mobile_code' => 'Silakan masukan kode konfirmasi sms yang anda terima di telpon genggam anda di bawah ini :',
+	'mobile_error_head' => 'Permintaan notifikasi mobile anda belum tersimpan !',
+	'mobile_ok_head' => 'Permintaan notifikasi mobile anda telah tersimpan !',
+	'settings_error' => 'Instalasi ini tidak terkonfigurasi untuk memroses notifikasi dengan benar',
+	'unsubscribe' => 'Anda menerima e-mail ini karena anda minta untuk dinotifikasi. Jika anda mau menghentikan pengiriman e-mail ini silakan ke',
+	'unsubscribed' => 'Anda tidak lagi akan dikirimi notifikasi dari',
+	'unsubscribe_failed' => 'Kami gagal membatalkan permintaan penghentian berlangganan anda. Mohon pastikan anda URL yg anda gunakan sudah benar.',
+	'verification_email_subject' => 'pesan - verifikasi',
+	'verify_code' => 'Anda tidak akan menerima pesan dari lokasi ini sebelum anda konfirmasikan permintaan anda.',
 	);
 ?>
--- id_ID/auth.php
+++ id_ID/auth.php
@@ -1,4 +1,33 @@
 <?php
 	$lang = array(
-	);
+	'email' => array(
+		'email' => 'Format alamat e-mail yang tercantum pada isian e-mail tidak valid ?',
+		'exists' => 'Maaf, alamat e-mail ini sudah digunakan pengguna lain.',
+		'length' => 'Panjang isian alamat e-mail harus minimal 4 dan tidak lebih dari 64 karakter.',
+		'required' => 'Isian e-mail harus diisi',
+	),
+	'name' => array(
+		'length' => 'Panjang isian nama lengkap harus minimal 3 dan tidak lebih dari 100 karakter.',
+		'required' => 'Isian nama lengkap harus diisi',
+		'standard_text' => 'Isian nama pengguna mengandung karakter yang terlarang.',
+	),
+	'password' => array(
+		'alpha_numeric' => 'Password hanya boleh diisi dengan angka dan huruf',
+		'length' => 'Panjang isian password harus minimal 5 dan tidak lebih dari 16 karakter.',
+		'matches' => 'Mohon masukkan password yang sama ke isian password kedua',
+		'required' => 'Isian password harus diisi',
+	),
+	'roles' => array(
+		'length' => 'Panjang isian peran harus minimal 5 dan tidak lebih dari 30 karakter.',
+		'required' => 'Anda harus tetapkan minimal satu peran',
+		'values' => 'Anda harus memilih peran admin ATAU pengguna',
+	),
+	'username' => array(
+		'admin' => 'Peran admin tidak boleh disunting',
+		'alpha' => 'Isian nama pengguna hanya boleh diisi dengan huruf',
+		'exists' => 'Maaf, nama pengguna ini sudah terpakai',
+		'length' => 'Panjang isian nama pengguna harus minimal 2 dan tidak lebih dari 16 karakter.',
+		'required' => 'Isian nama pengguna harus diisi',
+		'superadmin' => 'Peran admin super tidak boleh disunting',
+	));
 ?>
