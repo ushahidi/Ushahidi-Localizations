4dce29e4cce67469e5c7a7f013867091a93bdd83
ext-rrbaker <ext-rrbaker@cloud-ushahidi.hspheredns.com>
2011-01-20 13:18:14 -0600

    Updating translations for Akan (Ghana)

--- ja_JP/sharing.php
+++ ja_JP/sharing.php
@@ -1,4 +1,20 @@
 <?php
 	$lang = array(
-	);
+	'date' => '立ち入り日時',
+	'date_added' => '追加日時',
+	'last_access' => '最終アクセス',
+	'sharing_color' => array(
+		'length' => 'カラーフィールドは６文字でなければいけません。',
+		'required' => 'カラーフィールドは必須です。',
+	),
+	'sharing_name' => array(
+		'length' => '共有名は有効ではないようです。',
+		'required' => '共有名は必須です。',
+	),
+	'sharing_url' => array(
+		'exists' => 'サイトurlはすでに存在します。',
+		'length' => 'サイトurlは有効形式ではないようです',
+		'required' => 'サイトurlは必須です。',
+		'url' => 'サイトurlフィールドに有効な値が入っていないようです。',
+	));
 ?>
