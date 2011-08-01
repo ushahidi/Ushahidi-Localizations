cfe54ea8a6c64c84a1dfc4179b3b6eb677428e48
Boyan Yurukov <yurukov@gmail.com>
2011-08-01 18:26:04 +0200

    Added Bulgarian translation
    	new file:   bg_BG/alerts.php
    	new file:   bg_BG/auth.php
    	new file:   bg_BG/bug.php
    	new file:   bg_BG/category.php
    	new file:   bg_BG/comment.php
    	new file:   bg_BG/comments.php
    	new file:   bg_BG/contact.php
    	new file:   bg_BG/core.php
    	new file:   bg_BG/database.php
    	new file:   bg_BG/datetime.php
    	new file:   bg_BG/feedback.php
    	new file:   bg_BG/feeds.php
    	new file:   bg_BG/footer.php
    	new file:   bg_BG/form.php
    	new file:   bg_BG/imap.php
    	new file:   bg_BG/installer.php
    	new file:   bg_BG/layer.php
    	new file:   bg_BG/libraries.php
    	new file:   bg_BG/message.php
    	new file:   bg_BG/mhi.php
    	new file:   bg_BG/notifications.php
    	new file:   bg_BG/organization.php
    	new file:   bg_BG/report.php
    	new file:   bg_BG/reporters.php
    	new file:   bg_BG/roles.php
    	new file:   bg_BG/settings.php
    	new file:   bg_BG/sharing.php
    	new file:   bg_BG/stats.php
    	new file:   bg_BG/tooltips.php
    	new file:   bg_BG/ui_admin.php
    	new file:   bg_BG/ui_main.php
    	new file:   bg_BG/upgrade.php

---
+++ bg_BG/mhi.php
@@ -0,0 +1,18 @@
+<?php
+	$lang = array(
+	'contact_captcha' => array(
+		'default' => 'Моля въведете правилен код за сигурност',
+		'required' => 'Моля въведете код за сигурност',
+	),
+	'contact_email' => array(
+		'email' => 'Email адресът не изглежда да е правилен?',
+		'required' => 'Моля задайте правилен email адрес.',
+	),
+	'contact_message' => array(
+		'required' => 'Полето за съобщения трябва да съдържа нещо.',
+	),
+	'contact_subject' => array(
+		'length' => 'Заглавието трябва да е поне 3 символа.',
+		'required' => 'Заглавието е задължително.',
+	));
+?>
