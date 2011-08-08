ec6b27700e12cd6b67909da6b6a1923da4c123d0
Bariscan Camlidere <bariscancamlidere@gmail.com>
2011-08-08 16:15:23 +0300

    Edited tr_TR/contact.php via GitHub

--- tr_TR/contact.php
+++ tr_TR/contact.php
@@ -7,17 +7,17 @@
 	'contact_email' => array(
 		'email' => 'Girilen e-posta adresi geçerli değil',
 		'length' => 'E-posta adresi en az 4, en fazla 64 karakter uzunluğunda olmalıdır.',
-		'required' => 'Eğer onay kutusu işaretlenmiş ise e-posta alanı boş bırakılamaz.',
+		'required' => 'Kullanım koşullarını kabul ettiyseniz sisteme e-posta girmeniz gereklidir.',
 	),
 	'contact_message' => array(
 		'required' => 'Mesaj kısmı boş bırakılamaz.',
 	),
 	'contact_name' => array(
-		'length' => 'İsim en az 3 karakterden oluşmalıdır.',
+		'length' => 'İsim kısmı en az 3 karakterden oluşmalıdır.',
 		'required' => 'İsim kısmı boş bırakılamaz.',
 	),
 	'contact_subject' => array(
-		'length' => 'Başlık en az 3 karakterden oluşmalıdır.',
+		'length' => 'Başlık kısmı en az 3 karakterden oluşmalıdır.',
 		'required' => 'Başlık kısmı boş bırakılamaz.',
 	));
 ?>
