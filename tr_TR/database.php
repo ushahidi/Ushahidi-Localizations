36a784b06a31fed67e65e8630f908003ada966e9
Bariscan Camlidere <bariscancamlidere@gmail.com>
2011-08-08 16:43:29 +0300

    Edited tr_TR/database.php via GitHub

--- tr_TR/database.php
+++ tr_TR/database.php
@@ -1,4 +1,6 @@
 <?php
-	$lang = array(
-	);
+        $lang = array(
+        'error' => 'Veritabanı hatası: %s',
+        'table_not_found' => '%s tablosu veritabanı içerisinde bulunamadı. Lütfen Ushahidi veritabanının en son versiyonunu kullandığınızdan emin olun',
+        );
 ?>
