e3cbba8e4647fbe985f20a4a5221e5a81294e0c1
Bariscan Camlidere <bariscancamlidere@gmail.com>
2011-08-09 11:55:14 +0300

    Edited tr_TR/libraries.php via GitHub

--- tr_TR/libraries.php
+++ tr_TR/libraries.php
@@ -1,4 +1,22 @@
 <?php
-	$lang = array(
-	);
+$lang = array(
+              'akismet_cannot_connect' => 'Akismet sunucusuna bağlanılamadı',
+              'akismet_cannot_retrieve' => 'Akismet'ten gelen yanıta cevap verilemedi',
+              'api_library_not_found' => 'API kütüphanesi: <strong>%s</strong> için <strong>%s</strong> sınıfı bulunamadı. Lütfen API görev yolunuzu kontrol edin.',
+              'askimet_invalid_apikey' => 'Akismet API anahtarı geçerli değil',
+              'clickatell_fopen_error' => 'Hata! Fopen komutu ile gönderme metodu çalışmadı! <br> Lütfen PHP sunucusunun OpenSSL desteği olduğuna ve PHP versiyonunun 5.2'den yüksek olduğuna emin olun',
+              'clickatell_message_too_long' => 'Unicode mesajınız çok uzun! (Uzunluk=',
+              'clickatell_no_destination_address' => 'Lütfen gidilecek adresi belirtin (KİME)!',
+              'clickatell_no_sender_address' => 'Lütfen kaynak adresi belirtin (KİMDEN)!',
+              'clickatell_unicode_message_too_long' => 'Your unicode message is too long! (Current lenght=',
+              'clickatell_unsupported_method' => 'Unsupported sending method!',
+              'invalid_api_library' => 'The API library %s is invalid. All API libraries must be subclasses of %s',
+              'upgrade_directory_not_deleted' => 'Directory <code>%s</coded> could not be deleted',
+              'upgrade_extracting_error' => 'Error while extracting: <code>%s</code>',
+              'upgrade_failed' => 'Downloading the latest ushahidi failed. HTTP status code',
+              'upgrade_file_not_copied' => 'File <code>%s</coded> could not be copied',
+              'upgrade_file_not_deleted' => 'File <code>%s</coded> could not be deleted',
+              'upgrade_title' => 'Ushahidi upgrade script',
+              'upgrade_zip_error' => 'The downloaded ushahidi zip file <code>%s</code>, could not be written',
+);
 ?>
