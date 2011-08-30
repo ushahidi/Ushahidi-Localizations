4c7d1d431de37b05af8172b88555969cc3830411
Danil Antonov <danil.antonov@gmail.com>
2011-08-30 14:41:16 +0800

    Edited ru_RU/libraries.php via GitHub

--- ru_RU/libraries.php
+++ ru_RU/libraries.php
@@ -3,6 +3,7 @@
 	'akismet_cannot_connect' => 'Не удалось подключиться серверу Akismet',
 	'akismet_cannot_retrieve' => 'Не удалось получить ответ от сервера Akismet',
 	'askimet_invalid_apikey' => 'Akismet API ключ введен неверно',
+	'api_library_not_found' => 'API-библиотека: <strong>%s</strong>, для <strong>%s</strong> класса не найден. Пожалуйста, проверьте таблицу маршрутизации API-задач.',
 	'clickatell_fopen_error' => 'Метод отправки не поддерживается!<br>Проверьте поддерживает ли PHP OpenSSL и является ли версия PHP выше 5.2',
 	'clickatell_message_too_long' => 'Ваше сообщение слишком длинное! (Количество символов=',
 	'clickatell_no_destination_address' => 'Укажите адрес (куда)!',
