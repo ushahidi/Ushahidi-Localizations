e7b710883ce8cf7c4ac6604991fc2188c8fa25f1
Brian Herbert <brianherbert@gmail.com>
2012-04-03 18:11:23 -0400

    Added Rhodri's datetime updates in Welsh. HE is doing great work ;)

---
+++ cy_GB/datetime.php
@@ -0,0 +1,81 @@
+<?php
+	$lang = array(
+	'am' => 'am',
+	'friday' => array(
+		'abbv' => 'Gwe',
+		'full' => 'Gwener',
+	),
+	'monday' => array(
+		'abbv' => 'Llun',
+		'full' => 'Llun',
+	),
+	'pm' => 'pm',
+	'saturday' => array(
+		'abbv' => 'Sad',
+		'full' => 'Sadwrn',
+	),
+	'sunday' => array(
+		'abbv' => 'Sul',
+		'full' => 'Sul',
+	),
+	'thursday' => array(
+		'abbv' => 'Iau',
+		'full' => 'Iau',
+	),
+	'tuesday' => array(
+		'abbv' => 'Maw',
+		'full' => 'Mawrth',
+	),
+	'wednesday' => array(
+		'abbv' => 'Wed',
+		'full' => 'Wednesday',
+	),
+	'january' => array(
+		'abbv' => 'Ion',
+		'full' => 'Ionawr',
+	),
+	'february' => array(
+		'abbv' => 'Chw',
+		'full' => 'Chwefror',
+	),
+	'march' => array(
+		'abbv' => 'Maw',
+		'full' => 'Mawrth',
+	),
+	'april' => array(
+		'abbv' => 'Ebr',
+		'full' => 'Ebrill',
+	),
+	'may' => array(
+		'abbv' => 'Mai',
+		'full' => 'Mai',
+	),
+	'june' => array(
+		'abbv' => 'Meh',
+		'full' => 'Mehefin',
+	),
+	'july' => array(
+		'abbv' => 'Gor',
+		'full' => 'Gorffennaf',
+	),
+	'august' => array(
+		'abbv' => 'Awst',
+		'full' => 'Awst',
+	),
+	'september' => array(
+		'abbv' => 'Medi',
+		'full' => 'Medi',
+	),
+	'october' => array(
+		'abbv' => 'Hyd',
+		'full' => 'Hydref',
+	),
+	'november' => array(
+		'abbv' => 'Tach',
+		'full' => 'Tachwedd',
+	),
+	'december' => array(
+		'abbv' => 'Rhag',
+		'full' => 'Rhagfyr',
+	));
+?>
