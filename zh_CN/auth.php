f3b686fcb8b95920ec70bfb5f4c9b6469dd580b4
seiya027848 <seiya027848@gmail.com>
2012-05-31 21:25:51 +0800

    完成。

--- zh_CN/auth.php
+++ zh_CN/auth.php
@@ -12,31 +12,31 @@
 		'standard_text' => '用户名包含不合法字符',
 	),
 	'current_password' => array(
-		'length' => 'The password field must be at least 8 characters long.',
-		'login error' => 'Please check that you entered the correct email and password.',
-		'matches' => 'Please enter the same password in the two password fields.',
-		'required' => 'The password field is required.',
-		'alpha_dash' => 'The password field must have alphabetical characters, the # and @ symbols, numbers, underscores and dashes only',
-		'incorrect' => 'The current password you entered for your account is incorrect. Please try again.',
+		'length' => '密码必须有至少8个字符的长度。',
+		'login error' => '请您输入正确的电子邮件地址和密码。',
+		'matches' => '两次输入的密码不一致。',
+		'required' => '请输入密码。',
+		'alpha_dash' => '密码只能由字母，数字和符号＃、@、_以及-组成。',
+		'incorrect' => '您输入的账户密码是不正确的，请再试一次。',
 	),
 	'new_password' => array(
-		'length' => 'The password field must be at least 8 characters long.',
-		'login error' => 'Please check that you entered the correct email and password.',
-		'matches' => 'Please enter the same password in the two password fields.',
-		'required' => 'The password field is required.',
-		'alpha_dash' => 'The password field must have alphabetical characters, the # and @ symbols, numbers, underscores and dashes only',
+		'length' => '用户密码必须有至少8个字符的长度。',
+		'login error' => '请您输入正确的电子邮件地址和密码。',
+		'matches' => '两次输入的密码不一致。',
+		'required' => '请输入密码。',
+		'alpha_dash' => '密码只能由字母，数字和符号＃、@、_以及-组成。',
 	),
 	'password' => array(
-		'default' => 'There has been an error attempting to log you in.',
-		'length' => 'The password field must be at least 8 characters long.',
-		'login error' => 'Please check that you entered the correct email and password.',
-		'matches' => 'Please enter the same password in the two password fields.',
-		'required' => 'The password field is required.',
-		'riverid server down' => 'The authentication server is down. Please try again later.',
-		'alpha_dash' => 'The password field must have alphabetical characters, the # and @ symbols, numbers, underscores and dashes only',
+		'default' => '登录发生了一个错误。',
+		'length' => '账户密码必须有至少8个字符的长度。',
+		'login error' => '请您输入正确的电子邮件地址和密码。',
+		'matches' => '两次输入的密码不一致。',
+		'required' => '请输入密码。',
+		'riverid server down' => '访问认证服务器失败，请稍后再试。',
+		'alpha_dash' => '密码只能由字母，数字和符号＃、@、_以及-组成。',
 	),
 	'password_confirm' => array(
-		'matches' => '你输入的两次密码不一致',
+		'matches' => '两次输入的密码不一致。',
 	),
 	'resetemail' => array(
 		'email' => '邮件地址格式不正确',
@@ -44,7 +44,7 @@
 		'required' => '邮件是必填项',
 	),
 	'role' => array(
-		'superadmin_modify' => 'Only a superadmin may modify a superadmin or upgrade a user to admin.',
+		'superadmin_modify' => '只有超级管理员可以修改超级管理员或升级用户到管理员。',
 	),
 	'roles' => array(
 		'alpha_numeric' => '无效的角色格式',
@@ -54,12 +54,12 @@
 	),
 	'username' => array(
 		'admin' => '管理员帐户不能被修改',
-		'alpha_numeric' => 'The public profile URL field must only contain numbers and letters.',
+		'alpha_numeric' => '公开的URL只能包含数字和字母。',
 		'exists' => '对不起，这个用户名已被使用',
 		'length' => '用户名长度须在2至16个字符之间',
 		'login error' => '你输入的用户名不正确',
 		'required' => '用户名是必填项',
 		'superadmin' => '超级管理员帐户不能被修改',
-		'csrf' => 'Possible CSRF attack. Did you really mean to create/edit a user?'
+		'csrf' => '可能的CSRF(跨站请求伪造)攻击。你真的要创建/编辑用户？'
 	));
 ?>
