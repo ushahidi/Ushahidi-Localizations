5e8efa199bce720ebb434846795eebd1ee5311e8
seiya027848 <seiya027848@gmail.com>
2012-06-02 14:50:23 +0800

    完成

--- zh_CN/bug.php
+++ zh_CN/bug.php
@@ -1,23 +1,23 @@
 <?php
 	$lang = array(
 	'captcha' => array(
-		'default' => '请输入有效的验证码。',
-		'required' => '请输入验证码。',
+		'default' => '请输入有效的验证码',
+		'required' => '请输入验证码',
 	),
 	'email' => array(
-		'email' => '电子邮箱地址格式不正确。',
-		'length' => '电子邮箱地址长度须在4至64个字符之间。',
-		'required' => '打钩则电子邮箱地址为必填项。',
+		'email' => '电子邮箱地址格式不正确',
+		'length' => '电子邮箱地址长度须在4至64个字符之间',
+		'required' => '打钩则电子邮箱地址为必填项',
 	),
 	'error' => array(
-		'required' => '错误信息是必填项。',
+		'required' => '错误信息是必填项',
 	),
 	'subject' => array(
-		'length' => '标题长度须不少于3个字符。',
-		'required' => '标题是必填项。',
+		'length' => '标题长度须不少于3个字符',
+		'required' => '标题是必填项',
 	),
 	'yourname' => array(
-		'length' => '用户名长度须不少于3个字符。',
-		'required' => '用户名是必填项。',
+		'length' => '用户名长度须不少于3个字符',
+		'required' => '用户名是必填项',
 	));
 ?>
