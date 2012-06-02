c41864085d0d4851b5dc3db7a31efeb23e644262
seiya027848 <seiya027848@gmail.com>
2012-06-02 15:17:19 +0800

    完成

--- zh_CN/feeds.php
+++ zh_CN/feeds.php
@@ -2,14 +2,14 @@
 	$lang = array(
 	'date' => '日期',
 	'feed_name' => array(
-		'length' => '源名称的长度应在3至70个字符之间',
-		'required' => '请输入源的名称',
+		'length' => 'RSS提要名称的长度应在3至70个字符之间',
+		'required' => '请输入RSS提要的名称',
 	),
 	'feed_url' => array(
-		'required' => '请输入源的地址',
-		'url' => '请输入一个正确的 URL。比如：http://www.ushahidi.com',
+		'required' => '请输入RSS提要的源地址',
+		'url' => '请输入一个正确的URL。比如：http://www.ushahidi.com',
 	),
-	'source' => '源',
+	'source' => '来源',
 	'title' => '标题',
 	);
 ?>
