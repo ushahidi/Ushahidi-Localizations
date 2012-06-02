838e49dfbb2e9b4f5b4461d736e06ec14032a1a6
seiya027848 <seiya027848@gmail.com>
2012-06-02 14:53:54 +0800

    完成

--- zh_CN/contact.php
+++ zh_CN/contact.php
@@ -5,16 +5,16 @@
 		'required' => '请输入验证码',
 	),
 	'contact_email' => array(
-		'email' => '邮件地址格式不正确',
-		'length' => '邮件地址长度应为4至64个字符',
-		'required' => '打钩将邮件地址设为必填项',
+		'email' => '电子邮箱地址格式不正确',
+		'length' => '电子邮箱地址长度应为4至64个字符',
+		'required' => '打钩则电子邮箱地址为必填项',
 	),
 	'contact_message' => array(
 		'required' => '消息内容是必填项',
 	),
 	'contact_name' => array(
-		'length' => '姓名长度须不少于3个字母',
-		'required' => '名字是必填项',
+		'length' => '用户名长度须不少于3个字母',
+		'required' => '用户名是必填项',
 	),
 	'contact_subject' => array(
 		'length' => '主题长度必须超过3个字符',
