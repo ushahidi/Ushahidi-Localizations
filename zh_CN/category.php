3bea5aaf24c9c6e655d732221f532c222146bf2a
seiya027848 <seiya027848@gmail.com>
2012-06-02 14:49:50 +0800

    完成

--- zh_CN/category.php
+++ zh_CN/category.php
@@ -5,7 +5,7 @@
 		'required' => '颜色是必填项',
 	),
 	'category_description' => array(
-		'required' => '栏目的描述是必填项',
+		'required' => '类别的描述是必填项',
 	),
 	'category_image' => array(
 		'size' => '图片文件大小不能超过50K',
@@ -17,13 +17,13 @@
 		'required' => '标题是必填项',
 	),
 	'parent_id' => array(
-		'already_parent' => '栏目已在父栏目中存在。',
-		'exists' => '父栏目不存在',
-		'numeric' => '父栏目必须是数字',
-		'parent_trusted' => '父栏目不能是一个特殊的栏目。',
-		'required' => '父栏目是必填项',
-		'same' => '不能与父栏目相同',
-		'special' => '特殊栏目不能创建子栏目。',
+		'already_parent' => '要创建的子类别已在父类别中存在',
+		'exists' => '父类别不存在',
+		'numeric' => '父类别必须是数字',
+		'parent_trusted' => '父类别不能是一个有特殊性的类别',
+		'required' => '父类别是必填项',
+		'same' => '要创建的子类别不能与父类别相同',
+		'special' => '有特殊性的类别不能创建子类别',
 		
 	));
 ?>
