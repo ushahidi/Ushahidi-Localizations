5d808bc5fef6f4b3983ce10ee4c731d3c1f06304
seiya027848 <seiya027848@gmail.com>
2012-06-02 14:52:53 +0800

    完成

--- zh_CN/comments.php
+++ zh_CN/comments.php
@@ -3,18 +3,18 @@
 	'captcha' => array(
 		'default' => '验证码输入不正确',
 		'required' => '请输入验证码',
-		'valid' => 'You entered the wrong security code.',
+		'valid' => '输入了错误的验证码',
 	),
 	'comment_author' => array(
-		'length' => '姓名长度须不少于3个字母',
-		'required' => '名字是必填项',
+		'length' => '用户名长度须不少于3个字母',
+		'required' => '用户名是必填项',
 	),
 	'comment_description' => array(
 		'required' => '评论内容是必填项',
 	),
 	'comment_email' => array(
-		'email' => '邮件地址格式不正确',
-		'length' => '邮件地址长度应在4至64个字符之间',
-		'required' => '打钩将邮件地址设为必填项',
+		'email' => '电子邮箱地址格式不正确',
+		'length' => '电子邮箱地址长度应在4至64个字符之间',
+		'required' => '打钩则电子邮箱地址为必填项',
 	));
 ?>
