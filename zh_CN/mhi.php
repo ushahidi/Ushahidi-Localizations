0e57be4b326086d6c742c9e0dab5d0c4e2556767
seiya027848 <seiya027848@gmail.com>
2012-06-02 16:19:07 +0800

    完成

--- zh_CN/mhi.php
+++ zh_CN/mhi.php
@@ -1,12 +1,12 @@
 <?php
 	$lang = array(
 	'contact_captcha' => array(
-		'default' => '请输入有效的安全码',
-		'required' => '请输入安全码',
+		'default' => '请输入有效的验证码',
+		'required' => '请输入验证码',
 	),
 	'contact_email' => array(
-		'email' => '邮件地址格式不正确',
-		'required' => '请提供有效的电子邮件地址',
+		'email' => '电子邮箱地址格式不正确',
+		'required' => '请提供有效的电子邮箱地址',
 	),
 	'contact_message' => array(
 		'required' => '信息栏为必填项',
