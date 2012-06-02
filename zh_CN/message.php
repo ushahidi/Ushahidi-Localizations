29cbd13966c939aa3b5a9885c0276508b83abfa8
seiya027848 <seiya027848@gmail.com>
2012-06-02 16:17:59 +0800

    完成

--- zh_CN/message.php
+++ zh_CN/message.php
@@ -5,18 +5,18 @@
 		'required' => '请输入安全码',
 	),
 	'email' => array(
-		'email' => '邮件地址格式不正确',
-		'length' => '邮件地址长度须在4至64个字符之间',
-		'required' => '打钩将邮件地址设为必填项',
+		'email' => '电子邮箱地址格式不正确',
+		'length' => '电子邮箱地址长度须在4至64个字符之间',
+		'required' => '打钩则电子邮箱地址为必填项',
 	),
 	'message' => array(
 		'required' => '评论栏为必填项',
 	),
 	'name' => array(
-		'length' => '姓名长度须不少于3个字母',
-		'required' => '姓名栏为必填项',
+		'length' => '用户名长度须不少于3个字母',
+		'required' => '用户名为必填项',
 	),
 	'phone' => array(
-		'length' => '电话号码不可用',
+		'length' => '手机号码不正确',
 	));
 ?>
