2fec20b1f11b6a694532494a41b7bb7a8c25b4ce
Robbie Mackay <rm@robbiemackay.com>
2012-06-08 09:34:17 +1200

    Add script to pull latest translations from tx and convert to php

--- tools/run-all-php2po.sh
+++ tools/run-all-php2po.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Run php2po conversion on all languages
+# Only need to do this once for initial imports
 
 cd `dirname $0`/..
 for dir in `find . -maxdepth 1 -mindepth 1 -type d ! -name tools ! -name po ! -name .tx ! -name .git ! -name en_US ! -name pr_US -exec basename '{}' \;`
---
+++ tools/tx-po2php-sync.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Get latest translations from transifex, run po2php conversion on translations and push to github
+
+cd `dirname $0`/..
+
+echo "Pulling in latest changes from github..."
+git pull rjmackay
+
+echo "Pulling changes from transifex"
+# might need --force and --skip
+tx pull
+
+# generate the php files from transifex po files
+echo "Generating php files from translations..."
+tools/run-all-po2php.sh
+
+# add any new file generated
+git add .
+
+echo "Commiting changes..."
+git commit -am 'Daily update from transfex'
+
+echo "Pushing changes to github repo..."
+git push rjmackay
+
+echo "Done!"
