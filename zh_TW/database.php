8ea6be52f0bda1cc6b0a3c9b1f7fe728ccd0a364
seiya027848 <seiya027848@gmail.com>
2012-06-09 22:13:35 +0800

    更新

--- zh_TW/database.php
+++ zh_TW/database.php
@@ -1,6 +1,6 @@
 <?php
 	$lang = array(
-	'error' => 'Database error: %s',
-	'table_not_found' => 'Table "%s" cannot be found in the database. Please make sure you are using the latest version of the database for this version of Ushahidi',
+	'error' => '數據庫錯誤: %s',
+	'table_not_found' => '無法在數據庫中找到表"%s"。請確認您正在使用Ushahidi最新版本中的數據庫.',
 	);
 ?>
