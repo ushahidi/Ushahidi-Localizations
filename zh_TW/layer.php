66f8c2f0ee942fa295d9bd88b0031274c3fafa48
seiya027848 <seiya027848@gmail.com>
2012-06-09 22:40:31 +0800

    Update master

--- zh_TW/layer.php
+++ zh_TW/layer.php
@@ -5,8 +5,8 @@
 		'required' => '顏色欄位為必填',
 	),
 	'layer_file' => array(
-		'type' => 'The file field does not appear to contain a valid file. The only accepted formats are .KMZ, .KML.',
-		'valid' => 'The file field does not appear to contain a valid file',
+		'type' => '文件地址不包含有效的文件。只接受.KMZ，.KML類型的文件',
+		'valid' => '文件地址不包含有效的文件',
 	),
 	'layer_name' => array(
 		'length' => '姓名欄位需為3～80個字元',
@@ -14,7 +14,7 @@
 	),
 	'layer_url' => array(
 		'atleast' => '請至少輸入KML Url 或File',
-		'both' => 'You can\'t have both a KML file and a url',
+		'both' => '你無法同時使用一個KML文件和URL鏈接',
 		'url' => '請輸入有效的網址，例如：http://www.ushahidi.com/layerl.kml',
 	));
 ?>
