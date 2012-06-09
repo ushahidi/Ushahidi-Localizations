f2cf30f321a96173aa2edaf14afe9f30473b4822
seiya027848 <seiya027848@gmail.com>
2012-06-09 22:51:09 +0800

    Update master

--- zh_TW/page.php
+++ zh_TW/page.php
@@ -1,14 +1,14 @@
 <?php
 	$lang = array(
 	'page_title' => array(
-		'required' => 'Please enter a Page Title.',
-		'length' => 'Page Title must be at least 3 and no more than 150 characters long.'
+		'required' => '請輸入自定義頁面的標題',
+		'length' => '頁面標題需要不小於3個，但不超過150個的字符'
 	),
 	'page_tab' => array(
-		'required' => 'Please enter a Page Tab Name.'
+		'required' => '请输入页面的链接标签的名称.'
 	),
 	'page_description' => array(
-		'required' => 'Please enter a Page Description.'
+		'required' => '请输入自定义页面的内容.'
 	)
 	);
 
