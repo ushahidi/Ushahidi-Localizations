d3d4095f87098d00095db98d0bf7af18583785aa
seiya027848 <seiya027848@gmail.com>
2012-06-11 23:39:41 +0800

    Update master

--- zh_CN/layer.php
+++ zh_CN/layer.php
@@ -5,7 +5,7 @@
 		'required' => '颜色值是必填项',
 	),
 	'layer_file' => array(
-		'type' => '文件地址不包含有效的文件。只接受.KMZ，.KML类型的文件',
+		'type' => '文件地址不包含有效的文件。只接受.KMZ，.KML 类型的文件',
 		'valid' => '文件地址不包含有效的文件',
 	),
 	'layer_name' => array(
@@ -13,8 +13,8 @@
 		'required' => '姓名是必填项',
 	),
 	'layer_url' => array(
-		'atleast' => '需要填入KML，链接或文件',
-		'both' => '你无法同时使用一个KML文件和Url链接',
+		'atleast' => '需要填入 KML，链接或文件',
+		'both' => '你无法同时使用一个 KML 文件和 URL 链接',
 		'url' => '请输入一个正确的 URL。比如：http://www.ushahidi.com/layerl.kml',
 	));
 ?>
