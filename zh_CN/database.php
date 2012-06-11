5fd4527f935d1a2a1f44411fa0ec766e7d727c31
seiya027848 <seiya027848@gmail.com>
2012-06-11 23:35:37 +0800

    Update master

--- zh_CN/database.php
+++ zh_CN/database.php
@@ -1,6 +1,6 @@
 <?php
 	$lang = array(
 	'error' => '数据库错误：%s',
-	'table_not_found' => '无法在数据库中找到表"%s"。请确认您正在使用Ushahidi最新版本中的数据库。',
+	'table_not_found' => '无法在数据库中找到表"%s"。请确认您正在使用 Ushahidi 最新版本中的数据库。',
 	);
 ?>
