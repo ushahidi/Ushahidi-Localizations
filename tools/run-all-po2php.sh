0762d4f62d9eedd046500ed74426f3c749c331ab
Robbie Mackay <rm@robbiemackay.com>
2012-08-15 16:52:45 +1200

    Remove old tools

--- tools/run-all-po2php.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Run po2php conversion on all languages
-
-cd `dirname $0`/..
-for dir in `find . -maxdepth 1 -mindepth 1 -type d ! -name tools ! -name po ! -name .tx ! -name .git ! -name en_US ! -name pr_US -exec basename '{}' \;`
-do
-	echo "Processing $dir"
-	po2php -t en_US/ "po/po-$dir" "$dir/"
-done
