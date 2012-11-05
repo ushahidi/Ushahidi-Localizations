e34fa7a0c1a1070ecb81bf37a794ce0f1ed034fc
Angela Oduor <angela@ushahidi.com>
2012-11-05 11:28:45 +0300

    Adding i18n strings for form and form_field validations

--- en_US/form.php
+++ en_US/form.php
@@ -14,7 +14,7 @@ $lang = array(
 	'field_name' => array(
 		'length' => 'The Field Name must be at least 3 and no more than 100 characters long.',
 		'required' => 'Please enter Field Name.',
-		'duplicate' => 'The field name you entered already exists. Please enter another one.',
+		'duplicate' => 'The field name you entered already exists on this form. Please enter another one.',
 	) ,
 	'field_required' => array(
 		'between' => 'You have entered an invalid value for Field Required.',
@@ -38,6 +38,7 @@ $lang = array(
 	'form_title' => array(
 		'length' => 'The form name field must be at least 3 and no more than 100 characters long.',
 		'required' => 'Please enter the name of the form.',
+		'exists' => 'A form with that title already exists. Please choose another.',
 	)
 );
 ?>
